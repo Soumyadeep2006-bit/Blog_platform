9a9c748d79de8d006cc49ee81976239e95c5067f
Soumyadeep <dassoumyadeep2006@gmail.com>
2026-06-18 21:46:04 +0530

    Wrote auth service using ContextAPI

--- frontend/blog-frontend/src/context/AuthContext.tsx
+++ frontend/blog-frontend/src/context/AuthContext.tsx
@@ -15,5 +15,102 @@ interface AuthContextType{
     checkAuth: () => Promise<void>
 }
 
+//create context , default value undefined
+const AuthContext=createContext<AuthContextType|undefined>(undefined)
+
+//PROVIDER COMPONENT THAT WRAPS THE APP
+export const AuthProvider=({children}:{children:ReactNode})=>{
+    const [user,setUser]=useState<  User|null>(null)
+    const [isLoading,setIsLoading]=useState<boolean>(true)
+    const [error, setError] = useState<string | null>(null)
+
+      useEffect(() => {
+    checkAuth()
+  }, [])
+
+    const checkAuth=async()=>{
+        try{
+            setIsLoading(true)
+            const response=await userAPI.getCurrentUser()
+            setUser(response.data.data)
+            setError(null)
+        } catch(err){
+            setUser(null)
+            setError(null)//not an error if not logged in \
+
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+    const login =async(emailorusername:string,password:string)=>{
+        try{
+            setIsLoading(true)
+            const response=await authAPI.login({email:emailorusername,password})
+            setUser(response.data.data)
+            setError(null)
+
+        }catch (err:any){
+       setError(err.response?.data?.message||"Login falied")
+       throw(err)
+        }finally{
+            setIsLoading(false)
+        }
+
+    }
+
+    const register=async(username:string,email:string,fullName:string,password:string,avatar?:File)=>{
+        try{
+              const formData = new FormData()
+              formData.append("username", username)
+              formData.append("email", email)
+              formData.append("fullName", fullName)
+              formData.append("password", password)
+    if (avatar) {
+      formData.append("avatar", avatar)
+    }
+            setIsLoading(true)
+            const response=await authAPI.register(formData)
+            setUser(response.data.data)
+            setError(null)
+        }catch(err:any){
+            setError(err.response?.data?.message||"Registration Failed")
+            throw err
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+    const logout = async () => {
+    try {
+      setIsLoading(true)
+      await authAPI.logout()
+      setUser(null)
+      setError(null)
+    } catch (err: any) {
+      setError(err.response?.data?.message || "Logout failed")
+      throw err
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const value:AuthContextType={
+     user,
+    isLoading,
+    isAuthenticated: !!user,
+    error,
+    login,
+    register,
+    logout,
+    checkAuth
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+
+
+}
+
+   
 
 
